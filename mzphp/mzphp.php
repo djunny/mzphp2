53aafb8101d75dfab7755dcfb7aacb9f23d98106
djunny <duanjun@wandoujia.com>
2015-06-04 21:03:04 +0800

    one more FRAMEWORK_EXTEND_PATH / define FRAMEWORK_RUNTIMEFILE to make different runtime for multi site

--- mzphp/mzphp.php
+++ mzphp/mzphp.php
@@ -9,27 +9,33 @@ if(DEBUG > 0) {
 	foreach ($inc_files as $inc_file) {
 		include $inc_file;
 	}
-	if(defined('FRAMEWORK_EXTEND_PATH') && is_dir(FRAMEWORK_EXTEND_PATH)){
-		$inc_files = glob(FRAMEWORK_EXTEND_PATH.'*.class.php');
-		//load base first
-		foreach ($inc_files as $key=>$inc_file) {
-			if(preg_match('/\bbase_/i', $inc_file)){
-				include $inc_file;
-				unset($inc_files[$key]);
-			}
-		}
-		// load extends
-		if($inc_files){
-			foreach ($inc_files as $inc_file) {
-				include $inc_file;
+	if(defined('FRAMEWORK_EXTEND_PATH')){
+		//扩展目录用 | 隔开
+		$dirs = explode('|', FRAMEWORK_EXTEND_PATH);
+		foreach($dirs as $dir){
+			if($dir && is_dir($dir)){
+				$inc_files = glob($dir.'*.class.php');
+				//load base first
+				foreach ($inc_files as $key=>$inc_file) {
+					if(preg_match('/\bbase_/i', $inc_file)){
+						include $inc_file;
+						unset($inc_files[$key]);
+					}
+				}
+				// load extends
+				if($inc_files){
+					foreach ($inc_files as $inc_file) {
+						include $inc_file;
+					}
+				}
 			}
 		}
 	}
-	unset($inc_files, $inc_file);
+	unset($inc_files, $inc_file, $dirs, $dir);
 	
 } else {
 	// 语义同上段，优先读取应用定义的目录下的 runtime 文件
-	$runtimefile = FRAMEWORK_TMP_PATH.'_runtime.php';
+	$runtimefile = FRAMEWORK_TMP_PATH.(defined('FRAMEWORK_RUNTIMEFILE') ? FRAMEWORK_RUNTIMEFILE : '_runtime.php' );
 	if (!(@include($runtimefile))) {
 		$content = '';
 		if(!is_dir(FRAMEWORK_TMP_PATH)){
@@ -46,15 +52,22 @@ if(DEBUG > 0) {
 			}
 		}
 		// 加载扩展内核的文件
-		if(defined('FRAMEWORK_EXTEND_PATH') && is_dir(FRAMEWORK_EXTEND_PATH)){
-			$inc_files = glob(FRAMEWORK_EXTEND_PATH.'*.class.php');
-			foreach ($inc_files as $inc_file) {
-				$content .= php_strip_whitespace($inc_file);
+		if(defined('FRAMEWORK_EXTEND_PATH')){
+			
+			//扩展目录用 | 隔开
+			$dirs = explode('|', FRAMEWORK_EXTEND_PATH);
+			foreach($dirs as $dir){
+				if($dir && is_dir($dir)){
+					$inc_files = glob($dir.'*.class.php');
+					foreach ($inc_files as $inc_file) {
+						$content .= php_strip_whitespace($inc_file);
+					}
+				}
 			}
 		}
 		//写入 runtime 文件
 		file_put_contents($runtimefile, $content);
-		unset($content, $inc_files, $inc_file);
+		unset($content, $inc_files, $inc_file, $dirs, $dir);
 		//加载 runtime 
 		include $runtimefile;
 	}
